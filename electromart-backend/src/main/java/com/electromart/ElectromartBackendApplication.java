f0ee5bcb4cf385581b2599230f03f86089dee8d7
Harshagithu <gottipatiharsha21@gmail.com>
2026-08-10 11:18:44 +0530

    New Commit: Project Structure Updated

--- electromart-backend/src/main/java/com/electromart/ElectromartBackendApplication.java
+++ electromart-backend/src/main/java/com/electromart/ElectromartBackendApplication.java
@@ -2,8 +2,10 @@ package com.electromart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ElectromartBackendApplication {
 
 	public static void main(String[] args) {
